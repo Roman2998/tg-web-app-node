cab7f314c372e73b20fb078c19c22107a14dd1f3
Roman <roman290698@gmail.com>
2022-10-14 02:20:26 +0300

    second

--- index.js
+++ index.js
@@ -13,11 +13,18 @@ app.use(cors());
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
-    // const botOwner = '266833777';
     const text = msg.text;
 
     if(text === '/start') {
-        await bot.sendMessage(chatId, 'Заходи в наш автосревис по кнопке ниже', {
+        await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
+            reply_markup: {
+                keyboard: [
+                    [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
+                ]
+            }
+        })
+
+        await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
             reply_markup: {
                 inline_keyboard: [
                     [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
@@ -25,63 +32,38 @@ bot.on('message', async (msg) => {
             }
         })
     }
-    // if(msg?.web_app_data?.data) {
-    //     try {
-    //         const data = JSON.parse(msg?.web_app_data?.data)
-    //         console.log(data)
-    //         // await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
-    //         // await bot.sendMessage(chatId, 'Ваше имя: ' + data?.customerName);
-    //         // await bot.sendMessage(chatId, 'ваша машина: ' + data?.customerCar);
-    //
-    //         // setTimeout(async () => {
-    //         //     await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
-    //         // }, 3000)
-    //
-    //
-    //         // setTimeout(async () => {
-    //         //     await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
-    //
-    //
-    //             // await bot.answerWebAppQuery(botOwner, {
-    //             //     id: botOwner,
-    //             //     title: 'Успешная покупка',
-    //             // })
-    //
-    //         // }, 3000)
-    //
-    //     } catch (e) {
-    //         console.log(e);
-    //     }
-    // }
+
+    if(msg?.web_app_data?.data) {
+        try {
+            const data = JSON.parse(msg?.web_app_data?.data)
+            console.log(data)
+            await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
+            await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country);
+            await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street);
+
+            setTimeout(async () => {
+                await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
+            }, 3000)
+        } catch (e) {
+            console.log(e);
+        }
+    }
 });
 
 app.post('/web-data', async (req, res) => {
-    const {queryId, products = [], totalPrice , customerName, customerCar} = req.body;
-    console.log("body: ", req.body )
-    //const botOwner = '266833777'
+    const {queryId, products = [], totalPrice} = req.body;
     try {
         await bot.answerWebAppQuery(queryId, {
             type: 'article',
             id: queryId,
-            title: 'Успешная заявка',
+            title: 'Успешная покупка',
             input_message_content: {
-                message_text: `${customerName},${customerCar}  поздравляю вас, вы оформили на ремонт ${products.map(item => item.title).join(', ')}, на сумму от ${totalPrice}`
-                // message_text: `Работает ${products.map(item => item.title).join(', ')}, на сумму от ${totalPrice}`
+                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
             }
         })
-
-        // await bot.answerWebAppQuery(botOwner, {
-        //     type: 'article',
-        //     id: botOwner,
-        //     title: 'Успешная заявка',
-        //     input_message_content: {
-        //         message_text: ` ${customerName}, владелец авто ${customerCar}, оформил заявку на ремонт ремонт ${products.map(item => item.title).join(', ')}, на сумму от ${totalPrice}`
-        //     }
-        // })
-
-        return res.status(200).json({message: 'Success'});
+        return res.status(200).json({});
     } catch (e) {
-        return res.status(500).json({message: 'Error connecting to db', e})
+        return res.status(500).json({})
     }
 })
 
